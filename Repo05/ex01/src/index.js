ab0a388749d6eb5ee5a7412a07b17ee6db209fb5
AvdicAdnan <avdic_adnan@hotmail.com>
2022-11-11 09:44:23 +0100

    added repo05 ex1,2

---
+++ Repo05/ex01/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+  var myFirstName = "Adnan";
+  var myLastName = "Avdic";
+  var myHomeTown = "Sarajevo";
+  return { myFirstName, myLastName, myHomeTown };
+}
+
+console.log(main());
+module.exports = main;
