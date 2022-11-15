b32a57f2a2767c330fd335b41fb7f82d5ec14c5b
AvdicAdnan <avdic_adnan@hotmail.com>
2022-11-15 14:34:42 +0100

    fixed 15 added ex16

--- Repo09/ex15/src/index.js
+++ Repo09/ex15/src/index.js
@@ -14,12 +14,12 @@ function switchCase(letter) {
       animals = "stuff";
   }
 
-  console.log(animals);
+  return animals;
 }
 
-switchCase("a");
-switchCase("b");
-switchCase("c");
-switchCase("d");
-switchCase(4);
+console.log(switchCase("a"));
+console.log(switchCase("b"));
+console.log(switchCase("c"));
+console.log(switchCase("d"));
+console.log(switchCase(4));
 module.exports = switchCase;
