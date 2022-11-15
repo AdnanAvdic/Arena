d043598db3165f6966b253030c83f2671fc9f669
AvdicAdnan <avdic_adnan@hotmail.com>
2022-11-15 12:41:51 +0100

    added ex09

---
+++ Repo09/ex09/src/index.js
@@ -0,0 +1,15 @@
+function logicalOperator(num) {
+  if (num >= 20 || num <= 30) {
+    return "Out";
+  }
+  return "In";
+}
+
+console.log(logicalOperator(0));
+console.log(logicalOperator(9));
+console.log(logicalOperator(20));
+console.log(logicalOperator(23));
+console.log(logicalOperator(30));
+console.log(logicalOperator(31));
+console.log(logicalOperator(100));
+module.exports = logicalOperator;
