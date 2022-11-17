bc604101f9565958e023d808238d4903b5cf4032
AvdicAdnan <avdic_adnan@hotmail.com>
2022-11-17 18:53:10 +0100

    added ex08

--- Repo11/ex07/src/index.js
+++ Repo11/ex07/src/index.js
@@ -16,5 +16,12 @@ function multiplyArrayFunction(myArray) {
 
 console.log(multiplyArrayFunction([[2], [5, 6, 7], [8, 9]]));
 console.log(multiplyArrayFunction([[2.5, 2], [0.5, 0.2], [8]]));
+console.log(
+  multiplyArrayFunction([
+    [1, 2],
+    [3, 4, 5, 6],
+    [7, 8, 9],
+  ])
+);
 
 module.exports = multiplyArrayFunction;
