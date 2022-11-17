c63ac3bd55a52d8e44a9c032ded46c7599d3fbe2
AvdicAdnan <avdic_adnan@hotmail.com>
2022-11-17 15:23:14 +0100

    added ex04

---
+++ Repo11/ex04/src/index.js
@@ -0,0 +1,26 @@
+var temps = [
+  [31, 32, 19, 37],
+  [29, 27, 55, 36],
+  [17, 27, 42, 46],
+  [29, 52, 21, 64],
+  [91, 27, 31, 61],
+];
+
+function myArrayFunction() {
+  var averageDayTemp = [];
+  var sum = 0;
+
+  for (var i = 0; i < temps.length; i++) {
+    sum = 0;
+    for (var j = 0; j < temps[i].length; j++) {
+      sum += temps[i][j];
+    }
+    sum /= temps[i].length;
+    averageDayTemp.push(sum);
+  }
+
+  console.log(averageDayTemp);
+}
+
+myArrayFunction();
+module.exports = myArrayFunction;
