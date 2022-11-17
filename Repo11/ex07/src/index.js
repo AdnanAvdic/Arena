71856e4afc622cf52491caa1eea9314b6d61709a
AvdicAdnan <avdic_adnan@hotmail.com>
2022-11-17 16:02:43 +0100

    added ex07

---
+++ Repo11/ex07/src/index.js
@@ -0,0 +1,20 @@
+function multiplyArrayFunction(myArray) {
+  var arr = [];
+  var sum = 0;
+  var product = 1;
+
+  for (var i = 0; i < myArray.length; i++) {
+    for (var j = 0; j < myArray[i].length; j++) {
+      sum += myArray[i][j];
+      product *= myArray[i][j];
+    }
+  }
+  arr.push(product);
+  arr.push(sum);
+  return arr;
+}
+
+console.log(multiplyArrayFunction([[2], [5, 6, 7], [8, 9]]));
+console.log(multiplyArrayFunction([[2.5, 2], [0.5, 0.2], [8]]));
+
+module.exports = multiplyArrayFunction();
