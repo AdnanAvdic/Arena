13289d4a3e44c2cc83a46b4340258c1899a92b3c
AvdicAdnan <avdic_adnan@hotmail.com>
2022-11-18 11:49:08 +0100

    added ex02 ex03

---
+++ Repo12/ex02/src/index.js
@@ -0,0 +1,11 @@
+function myCounter(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = myCounter(n - 1);
+    countArray.push(n);
+    return countArray;
+  }
+}
+
+console.log(myCounter(5));
